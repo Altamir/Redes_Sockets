0cc321b8e872551730aa28e6604730ca844c5f7c
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2015-03-26 14:27:47 -0300

    Criando Servidor e Cliente

--- src/com/sockets/Servidor.java
+++ src/com/sockets/Servidor.java
@@ -1,10 +1,24 @@
 package com.sockets;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
 public class Servidor {
 
-	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		ServerSocket servidor = new ServerSocket(12345);
+		System.out.println("Porta12345aberta!");
+		Socket cliente = servidor.accept();
+		System.out.println("Novaconexãocomocliente"
+				+ cliente.getInetAddress().getHostAddress());
+		Scanner s = new Scanner(cliente.getInputStream());
+		while (s.hasNextLine()) {
+			System.out.println(s.nextLine());
+		}
+		s.close();
+		servidor.close();
+		cliente.close();
 	}
 
 }
